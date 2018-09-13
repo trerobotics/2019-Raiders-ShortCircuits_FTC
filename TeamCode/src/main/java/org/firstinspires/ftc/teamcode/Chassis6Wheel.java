1242829c8144d20895a2d16e253d564dadd18873
HS East Robotics <hseastrobotics@gmail.com>
2018-09-13 15:18:36 -0400

    init commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chassis6Wheel.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by HS East Robotics on 9/13/2018.
+ */
+
+public class Chassis6Wheel extends Hardware {
+
+    public DcMotor  leftDrive   = null;
+    public DcMotor  rightDrive  = null;
+
+    public void init (HardwareMap hwMap){
+        leftDrive  = hwMap.get(DcMotor.class,"left_drive");
+        rightDrive = hwMap.get(DcMotor.class, "right_drive");
+
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+}
